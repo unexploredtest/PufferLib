43250ea76b66d59fae20e4950de3109baed9c4d7
unexploredtest <unexploredtest@tutanota.com>
2025-07-02 12:21:06 +0330

    Comment unnecessary code

--- pufferlib/ocean/speed_hockey/speed_hockey.h
+++ pufferlib/ocean/speed_hockey/speed_hockey.h
@@ -382,29 +382,29 @@ void c_step(SpeedHockey* env) {
 
     // Handle collisions with paddles
     if(check_collision_behind(env, player_index)) {
-        // float dx = env->ball_x - env->paddle_x_behind_offset;
-        // float dy = env->ball_y - env->players[player_index].behind_paddle_y;
+        // // float dx = env->ball_x - env->paddle_x_behind_offset;
+        // // float dy = env->ball_y - env->players[player_index].behind_paddle_y;
 
-        if(dx < dy) {
-            env->ball_vx = -env->ball_vx;
-        } else if(dx > dy) {
-            env->ball_vy = -env->ball_vy;
-        } else {
-            env->ball_vx = -env->ball_vx;
-            env->ball_vy = -env->ball_vy;
-        }
+        // if(dx < dy) {
+        //     env->ball_vx = -env->ball_vx;
+        // } else if(dx > dy) {
+        //     env->ball_vy = -env->ball_vy;
+        // } else {
+        //     env->ball_vx = -env->ball_vx;
+        //     env->ball_vy = -env->ball_vy;
+        // }
     } else if(check_collision_front(env, player_index)) {
-        float dx = env->ball_x - env->paddle_x_front_offset;
-        float dy = env->ball_y - env->players[player_index].front_paddle_y;
+        // float dx = env->ball_x - env->paddle_x_front_offset;
+        // float dy = env->ball_y - env->players[player_index].front_paddle_y;
 
-        if(dx < dy) {
-            env->ball_vx = -env->ball_vx;
-        } else if(dx > dy) {
-            env->ball_vy = -env->ball_vy;
-        } else {
-            env->ball_vx = -env->ball_vx;
-            env->ball_vy = -env->ball_vy;
-        }
+        // if(dx < dy) {
+        //     env->ball_vx = -env->ball_vx;
+        // } else if(dx > dy) {
+        //     env->ball_vy = -env->ball_vy;
+        // } else {
+        //     env->ball_vx = -env->ball_vx;
+        //     env->ball_vy = -env->ball_vy;
+        // }
     }
 
     // clip ball
