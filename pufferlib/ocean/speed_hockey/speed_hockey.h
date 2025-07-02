23cdd78c78379260cd516b45257957a8ad351660
unexploredtest <unexploredtest@tutanota.com>
2025-07-02 20:50:34 +0330

    Return collisions

--- pufferlib/ocean/speed_hockey/speed_hockey.h
+++ pufferlib/ocean/speed_hockey/speed_hockey.h
@@ -422,7 +422,7 @@ void c_step(SpeedHockey* env) {
     }
 
     // Handle collisions with paddles
-    // if(check_collision_behind(env, player_index)) {
+    if(check_collision_behind(env, player_index)) {
     //     // // float dx = env->ball_x - env->paddle_x_behind_offset;
     //     // // float dy = env->ball_y - env->players[player_index].behind_paddle_y;
 
@@ -434,7 +434,7 @@ void c_step(SpeedHockey* env) {
     //     //     env->ball_vx = -env->ball_vx;
     //     //     env->ball_vy = -env->ball_vy;
     //     // }
-    // } else if(check_collision_front(env, player_index)) {
+    } else if(check_collision_front(env, player_index)) {
     //     // float dx = env->ball_x - env->paddle_x_front_offset;
     //     // float dy = env->ball_y - env->players[player_index].front_paddle_y;
 
@@ -446,7 +446,7 @@ void c_step(SpeedHockey* env) {
     //     //     env->ball_vx = -env->ball_vx;
     //     //     env->ball_vy = -env->ball_vy;
     //     // }
-    // }
+    }
 
     // clip ball
     env->ball_x = fminf(fmaxf(env->ball_x, 0), env->width - env->ball_width);
